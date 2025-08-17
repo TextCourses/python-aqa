e1cee22b1544e58331174cb6bc35fd95276fdf41
TextCourses <textcourses@gmail.com>
2025-08-17 12:53:54 +0400

    add test api with env

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,7 +1,15 @@
 # tests/test_example.py
+import os
+import requests
+
+URL = os.getenv("APP_URL")
 
 def test_sum():
     assert 1 + 1 == 2
 
 def test_upper():
     assert "abc".upper() == "ABC"
+
+def test_api():
+    req = requests.get(f"{URL}/objects")
+    assert req.status_code == 200
