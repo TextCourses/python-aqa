a8aa5f11025801ce957afe0372ba2d9b7ac7b1c4
TextCourses <textcourses@gmail.com>
2025-08-18 09:47:20 +0400

    add work with secrets

--- tests/test_example.py
+++ tests/test_example.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 URL = os.getenv("APP_URL")
+AUTH_TOKEN = os.environ["AUTH_TOKEN"]
 
 def test_sum():
     assert 1 + 1 == 2
@@ -15,3 +16,11 @@ def test_upper():
 def test_api():
     req = requests.get(f"{URL}/objects")
     assert req.status_code == 200
+
+def test_api_auth():
+    headers = {
+        "Accept": "application/json",
+        "Authorization": AUTH_TOKEN
+    }
+    response = requests.get("https://httpbin.org/bearer", headers=headers)
+    assert response.status_code == 200
